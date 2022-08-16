be0aca7d2995a9852f0cc9c6150dff4cefb23c98
Oleksandr Volik <polskasasha@gmail.com>
2022-08-16 05:49:31 +0300

    finish-hw-11

--- src/createmarkup.js
+++ src/createmarkup.js
@@ -3,35 +3,38 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const createSmallCardsMarkup = array => {
+  galleryContainer.innerHTML = '';
   const arrayRender = array.reduce(
     (
       acc,
       { webformatURL, largeImageURL, tags, likes, views, comments, downloads }
     ) =>
       acc +
-      `<a href = "${largeImageURL}" class = "gallery__link">
-      <div class="gallery__item">
-      <img src="${webformatURL}" alt="${tags}" loading="lazy" class = "gallery__image" />
+      `<div class = "gallery__item">
+      <a href = "${largeImageURL}" class = "gallery__link">
+      <div>
+      <img src="${webformatURL}" alt="${tags}" loading="lazy" class = "gallery__image"/>
       <div class="info">
         <p class="info-item">
           <b>Likes: ${likes}
 </b>
         </p>
         <p class="info-item">
-          <b>Views:${views}
+          <b>Views: ${views}
  </b>
         </p>
         <p class="info-item">
-          <b>Comments:${comments}
+          <b>Comments: ${comments}
 </b>
         </p>
         <p class="info-item">
-          <b>Downloads:${downloads}
+          <b>Downloads: ${downloads}
 </b>
         </p>
       </div>
     </div>
-      </a> `,
+      </a> 
+      </div>`,
     ''
   );
   galleryContainer.insertAdjacentHTML('beforeend', arrayRender);
